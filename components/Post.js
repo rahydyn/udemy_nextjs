1c2f126a0a14a99797ea2601918b8a1f4dbea0f9
rahydyn <kanaokasatoru@gmail.com>
2022-09-22 23:45:55 +0900

    second commit

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Post = ({ post }) => {
+export default function Post({ post }) {
   return (
     <div>
       <span>{post.id}</span>
@@ -11,6 +11,4 @@ const Post = ({ post }) => {
       </Link>
     </div>
   );
-};
-
-export default Post;
+}
